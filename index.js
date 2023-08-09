e7730b94a477ff29edef73882e34b3fdb71b21e7
Sivasish Praharaj <98220976+Sivasish48@users.noreply.github.com>
2023-08-09 18:42:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const accordian = document.getElementsByClassName("content-container");
+
+for( i = 0; i<= accordian.length;i++) {
+   
+    accordian[i].addEventListener('click' , function(){
+        this.classList.toggle('active');
+    })
+};
+
